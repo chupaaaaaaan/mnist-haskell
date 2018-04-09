e87e34238837bb9b1a41ca7bf6089c377c7592eb
CHPN (@chupaaaaaaan) <jacobitebeerlove@yahoo.co.jp>
2018-04-09 14:40:38 +0000

    accuracy計算用のデータ読み込みを、whenの中に移動 頭に書くと、一度読み込んだらずっと開放されずにいるため（後で使うかもしれないから）、 スペースリークの原因になる。 whenの中にすると、遅延読み込み・開放が効いてメモリ消費量が定数になる

--- mnist-list.hs
+++ mnist-list.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE Strict, StrictData, GADTs #-}
--- {-# LANGUAGE GADTs #-}
+-- {-# LANGUAGE Strict, StrictData, GADTs #-}
+{-# LANGUAGE GADTs #-}
 
 import Control.Monad
 import Control.Exception
@@ -43,6 +43,9 @@ testDataNum = 10000
 
 bsize :: Int -- mini batch size
 bsize = 100
+
+bsize_acc :: Int -- mini batch size for calc accuracy
+bsize_acc = 1000
 
 epochUnit :: Int
 epochUnit = (trainDataNum + bsize -1) `div` bsize
@@ -186,21 +189,13 @@ main = do
   -- variables
   refp <- newIORef (w2, b2, w3, b3)
   refm <- newIORef (0.0, 0.0, 0.0, 0.0)
-  refac_train <- newIORef 0
-  refac_test <- newIORef 0
 
   -- fetch dataset
   shuffles <- toDataSet Shuffle
-  trains <- toDataSet Train
-  tests <- toDataSet Test
 
   -- split to mini batch
   let mbs = slice2list bsize shuffles
       loop = zip (take trainNum [0..]) mbs
-      train_mbs = slice2list bsize trains
-      loop_train = zip (take trainNum [0..]) train_mbs
-      test_mbs = slice2list bsize tests
-      loop_test = zip (take trainNum [0..]) test_mbs
 
   -- learning steps
   forM_ loop $ \(i,mb) -> do
@@ -219,36 +214,30 @@ main = do
 
     -- output error
     p' <- readIORef refp
-    let dataline = show i ++ " " ++ show (accuracy_count p' mb) ++ " " ++ show (loss p' mb)
+    let dataline = show i ++ " " ++ show (accuracy_count p' mb) ++ " " ++ show (loss p' mb) ++ "\n"
     appendFile "error.dat" dataline
-    putStrLn dataline
 
     -- output accuracy
-    -- when ((i+1) `mod` epochUnit == 0) $ do
-    when (i == 0) $ do
-      writeIORef refac_train 0
-      writeIORef refac_test 0
+    when ((i+1) `mod` epochUnit == 0) $ do
+      trains <- toDataSet Train
+      tests  <- toDataSet Test
+      refac_train <- newIORef 0
+      refac_test  <- newIORef 0
+      let mbs_train = slice2list bsize_acc trains
+          mbs_test  = slice2list bsize_acc tests
+          loop_train = zip (take trainNum [0..]) mbs_train
+          loop_test  = zip (take trainNum [0..]) mbs_test
 
-      -- forM_ test_mbs $ \mb -> modifyIORef refac_test (+ accuracy_count p' mb)
-      forM_ loop_test $ \(i,mb) -> do
-        ac <- readIORef refac_test
-        putStrLn $ show i ++ " " ++ show ac
-        modifyIORef refac_test (+ accuracy_count p' mb)
 
-      -- forM_ train_mbs $ \mb -> modifyIORef refac_train (+ accuracy_count p' mb)
-      forM_ loop_train $ \(i,mb) -> do
-        ac <- readIORef refac_train
-        putStrLn $ show i ++ " " ++ show ac
-        modifyIORef refac_train (+ accuracy_count p' mb)
+      forM_ train_mbs $ \mb -> modifyIORef refac_train (+ accuracy_count p' mb)
+      forM_ test_mbs  $ \mb -> modifyIORef refac_test  (+ accuracy_count p' mb)
 
       ac_train <- readIORef refac_train
-      ac_test <- readIORef refac_test
-      print ac_train
+      ac_test  <- readIORef refac_test
 
       let dataline = show ((i+1) `div` epochUnit) ++ " " ++
                      show (ac_train / fromIntegral trainDataNum) ++ " " ++
-                     show (ac_test / fromIntegral testDataNum)
+                     show (ac_test  / fromIntegral testDataNum)  ++ "\n"
       appendFile "accuracy.dat" dataline
-      putStrLn dataline
 
   return ()
